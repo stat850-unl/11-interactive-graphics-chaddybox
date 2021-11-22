900ee74e79f7cd035889f0e23e891610dfcbb40e
chaddybox <chad.jenkins@huskers.unl.edu>
2021-11-22 14:55:54 -0600

    App updates

--- app.R
+++ app.R
@@ -1,49 +1,84 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+# Load R packages
 library(shiny)
+library(shinythemes)
+library(ggplot2)
+library(dplyr)
+library(shinydashboard)
+library(reactable)
+library(scales)
+library(plotly)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# Read in data and eliminate duplicates in cocktail categories
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+cocktails <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/cocktails.csv')
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+drink_category <- unique(cocktails$category)
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
+drink_ingredient <- as.data.frame(unique(cocktails$ingredient))
+colnames(drink_ingredient) = "ingredient"
+drink_ingredient <- as.data.frame(sort(drink_ingredient$ingredient))
+colnames(drink_ingredient) = "ingredient"
 
-# Define server logic required to draw a histogram
+# Define UI
+ui <- fluidPage(theme = shinytheme("slate"),
+                navbarPage(
+                  "CocktailR",
+                  tabPanel("Cocktail Complexity",
+                           sidebarPanel(
+                             inputID = "category_choice",
+                             radioButtons("checkGroup",
+                                                h4("Select a Category"),
+                                                choices = drink_category),
+                             
+                           ), 
+                           mainPanel(
+                             h4("Complexity of Cocktail Categories"),
+                             plotOutput("plot1"),
+                             
+                           ) 
+                           
+                  ), 
+                  tabPanel("Cocktail Recipes by Ingredient",
+                           sidebarPanel(
+                             inputID = "ingredient_choice",
+                             selectizeInput("select",
+                                                h4("Select an Ingredient"),
+                                                choices = drink_ingredient),
+                           ),
+                           mainPanel(
+                             h4("Recipes Table"),
+                             tableOutput("recipes")
+                           )
+                  )
+                ) 
+) 
+
+
+# Define server function  
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+  
+  output$plot1 <- renderPlot({
+    ggplot(data = subset(cocktails, cocktails$category %in% input$checkGroup), mapping = aes(x = ingredient_number), stat = "count") +
+      geom_histogram() +
+      ggtitle("How Many Ingredients are Required?") +
+      xlab("Number of Ingredients") +
+      ylab("Number of Recipes") +
+      scale_x_continuous(breaks=pretty_breaks()) +
+      scale_x_continuous(breaks=pretty_breaks())
+  })
+  
+  tab <- reactive({
+    
+    cocktails %>%
+      filter(ingredient == input$select)
+    
+  })
+  
+  output$recipes <- renderTable({
+    tab()
+  })
 }
 
-# Run the application 
+
+# Create Shiny object
 shinyApp(ui = ui, server = server)
